84c0e9faa73cdf1a71a5376ae1cdf840a1f9cfd7
Rinat <hakimovrinat@gmail.com>
2023-10-05 18:55:36 +0700

    Task4_att1

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -1,22 +1,81 @@
 package ru.netology.nmedia
 
 import android.os.Bundle
+import android.os.SystemClock
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import ru.netology.nmedia.ui.theme.NMediaTheme
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import ru.netology.nmedia.databinding.ActivityMainBinding
+import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.funcs.countToString
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val post = Post(
+            id = 1,
+            author = "Нетология. Университет интернет-профессий будущего",
+            published = "21 мая в 18:36",
+            content = "Привет, это новая Нетология! " +
+                    "Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. " +
+                    "Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. " +
+                    "Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. " +
+                    "Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
+            likedByMe = false,
+            likes = 1114,
+            shares = 10
+        )
+        with(binding) {
+            author.text = post.author
+            publishedTime.text = post.published
+            content.text = post.content
+            likeCount.text = countToString(post.likes)
+
+            if (post.likedByMe) {
+                likeIv?.setImageResource(R.drawable.ic_baseline_thumb_up_24pd)
+            }
+
+            likeIv.setOnClickListener {
+                post.likedByMe = !post.likedByMe
+
+                post.likes += if (post.likedByMe) 1 else -1
+                likeCount.text = countToString(post.likes)
+
+                likeIv?.setImageResource(
+                    if (post.likedByMe) R.drawable.ic_baseline_thumb_up_24pd else R.drawable.baseline_thumb_up_off_alt_24dp
+                )
+            }
+
+            shareCount.text = countToString(post.shares)
+
+            shareIv.setOnClickListener {
+                post.shares += 1
+                shareCount.text = countToString(post.shares)
+                shareIv.setImageResource(R.drawable.baseline_share_blue_24dp)
+
+                GlobalScope.launch {
+                    delay(1500) // In ms
+                    //Code after sleep
+                    shareIv.setImageResource(R.drawable.ic_baseline_share_24dp)
+                }
+            }
+            /*
+            binding.root.setOnClickListener{ println("root") }
+            binding.likeIv.setOnClickListener{ println("likeIv")}
+            binding.avatar.setOnClickListener{ println("avatar") }
+             */
+        }
+            /*
         setContentView(R.layout.activity_main)
-        /*
+        findViewById<ImageButton>(R.id.likeIv).setOnClickListener{
+            println("Click")
+        }
+        */
+            /*
         setContent {
             NMediaTheme {
                 // A surface container using the 'background' color from the theme
---
+++ app/src/main/java/ru/netology/nmedia/funcs/funcs.kt
@@ -0,0 +1,13 @@
+package ru.netology.nmedia.funcs
+
+import kotlin.math.log
+import kotlin.math.pow
+
+fun countToString (x: Long): String {
+    when (log(x+0.0,10.0).toInt()) {
+        0,1,2 -> return x.toString()
+        3,4,5 -> return ("%.2f".format((x+0.0)*(10.0).pow(-3.0))) +"K"
+        6,7,8 -> return ("%.2f".format((x+0.0)*(10.0).pow(-6.0))) +"M"
+        else -> return if (x>=1_000_000) ">=1B" else "error"
+    }
+}
